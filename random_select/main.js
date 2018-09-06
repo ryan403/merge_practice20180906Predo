73e1f5b37812b25815a067a61520d3643bb52fa5
Ryan Chung <ryan.jone@gmail.com>
2018-09-06 23:09:47 +0800

    first commit

---
+++ random_select/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("input").click(function()
+    {  
+        var numberOfListItem = $("#choices li").length;
+        var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
+        
+        $("h1").text($("#choices li").eq(randomChildNumber).text());  
+    });
+});
+
